09617f46f5986358006199168942093e23b91029
szuhaydv <szuhaydv@gmail.com>
2024-10-11 17:20:36 +0200

    change to 3 digits number

--- reverse.c
+++ reverse.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-    int digit1, digit2;
-    printf("Enter a two digit number: ");
-    scanf("%1d%1d", &digit1, &digit2);
-    printf("The reversal is: %1d%1d\n", digit2, digit1);
+    int digit1, digit2, digit3;
+    printf("Enter a three digit number: ");
+    scanf("%1d%1d%1d", &digit1, &digit2, &digit3);
+    printf("The reversal is: %1d%1d%1d\n", digit3, digit2, digit1);
     return 0;
 }
