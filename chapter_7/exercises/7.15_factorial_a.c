e6d3191e1270d74b9b254a58e5170aad0db427ed
szuhaydv <szuhaydv@gmail.com>
2024-10-27 15:22:33 +0100

    7.15a-g

--- chapter_7/exercises/7.15_factorial_a.c
+++ chapter_7/exercises/7.15_factorial_a.c
@@ -1,6 +1,8 @@
 #include <limits.h>
 #include <stdio.h>
 
+// Change the MAX type and the format specifier for other values
+
 int main(void) {
     short prod = 1, n = 2;
     while (prod < SHRT_MAX / n) {
